7b20c790ca5344c8b14442fa037f3353438bb6eb
cterrazas2 <12790854+cterrazas2@users.noreply.github.com>
2022-10-23 23:56:56 -0400

    fix api url if in dev/prod

--- pyvet/creds.py
+++ pyvet/creds.py
@@ -3,8 +3,16 @@
 ####################
 import os
 
-DEV = os.getenv("DEV", True)
-PROD = os.getenv("PROD", False)
+DEV = os.getenv("DEV", "True").lower() in (
+    "true",
+    "t",
+    "1",
+)
+PROD = os.getenv("PROD", "False").lower() in (
+    "true",
+    "t",
+    "1",
+)
 
 VA_PROD_API = os.getenv("VA_PROD_API", "https://api.va.gov/services/")
 VA_SANDBOX_API = os.getenv("VA_SANDBOX_API", "https://sandbox-api.va.gov/services/")
