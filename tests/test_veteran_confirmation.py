21f3e1f51ca3a269dcf82a6599203b799fcd83e0
cterrazas2 <12790854+cterrazas2@users.noreply.github.com>
2023-04-10 18:22:19 -1000

    Bump veteran confirmation API to v1, update changes for ssn

--- tests/test_veteran_confirmation.py
+++ tests/test_veteran_confirmation.py
@@ -12,30 +12,38 @@ mock_confirmed = dict(veteran_status="confirmed")
 class TestVeteranConfirmation(unittest.TestCase):
     def setUp(self):
         self.headers = creds.API_KEY_HEADER
-        self.confirmation_url = creds.VA_SANDBOX_API + "veteran_confirmation/v0/"
+        self.confirmation_url = creds.VA_SANDBOX_API + "veteran-confirmation/v1/"
 
     def test_get_status(self, mock_post):
         mock_post.return_value.status_code = 200
         mock_post.return_value.json.return_value = mock_confirmed
         assert mock_post.headers == self.headers
         vet_status = get_status(
-            ssn="796-13-0115",
-            first_name="Tamara",
-            last_name="Ellis",
-            birth_date="1967-06-19",
-            middle_name="E",
-            gender="F",
+            first_name="Alfredo",
+            last_name="Armstrong",
+            birth_date="1993-06-08",
+            middle_name="M",
+            gender="M",
+            street_address="17020 Tortoise St",
+            city="Round Rock",
+            zip_code="78664",
+            state="TX",
+            country="USA",
         )
         self.assertDictEqual(vet_status, mock_confirmed)
         mock_post.assert_called_once_with(
             self.confirmation_url + "status",
             json=dict(
-                ssn="796-13-0115",
-                first_name="Tamara",
-                last_name="Ellis",
-                birth_date="1967-06-19",
-                middle_name="E",
-                gender="F",
+                firstName="Alfredo",
+                lastName="Armstrong",
+                birthDate="1993-06-08",
+                middleName="M",
+                gender="M",
+                streetAddressLine1="17020 Tortoise St",
+                city="Round Rock",
+                zipCode="78664",
+                state="TX",
+                country="USA",
             ),
         )
 
